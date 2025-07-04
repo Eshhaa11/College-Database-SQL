25a8e3f8cd5c7d0cce61cf38b65e68dccc0eac07
Esha Patel <Patellesha11@gmail.com>
2025-07-04 14:32:32 +0300

    Done

--- create_college_db.sql
+++ create_college_db.sql
@@ -3,5 +3,14 @@ CREATE TABLE departments (
     name TEXT NOT NULL
 );
 
+CREATE TABLE instructors (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    department_id INTEGER
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+
+
 
 
